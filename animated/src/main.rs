ed927eb15e30a952aed96cbe4277d67cdf0cc6c4
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-06-24 18:05:07 -0700

    Update main.rs

--- animated/src/main.rs
+++ animated/src/main.rs
@@ -1,4 +1,5 @@
 use ::TerimalRtdm::*;
+use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
@@ -17,6 +18,25 @@ fn main() {
             .pressed(&mut app, KeyType::Q)
         {
             break;
+        }
+
+        // animation
+        loop {
+            collect_presses(&mut app);
+
+            Text::new()
+                .foreground(Color::Black)
+                .style(Style::Bold)
+                .show(&mut app, "TerimalRtdm", pos!(0, 0));
+            sleep(Duration::from_millis(500));
+
+            if Key::o()
+                .no_clear()
+                .case_sen(true)
+                .pressed(&mut app, KeyType::Q)
+            {
+                break;
+            }
         }
 
         render(&app);
