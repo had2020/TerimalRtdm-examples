8611e3940de9ad542cfd39819dde68b41a055aed
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-06-25 16:23:58 -0700

    updates

---
+++ blank/src/main.rs
@@ -0,0 +1,22 @@
+use ::TerimalRtdm::*;
+
+fn main() {
+    let mut app = App::new();
+    clear(&mut app);
+    raw_mode(true); // enabled for correct showing of elements at specific positions.
+    show_cursor(false); // By default it is set to show. The cursor is off, since we don't need to move it.
+
+    loop {
+        Text::new().show(&mut app, "Hello world", pos!(0, 0));
+
+        // Example exit key
+        if Key::o().pressed(&mut app, KeyType::Esc) {
+            break;
+        }
+
+        render(&app);
+        collect_presses(&mut app);
+    }
+
+    restore_terminal(); // Should be done at any exit of your program to restore the terminal defaults.
+}
